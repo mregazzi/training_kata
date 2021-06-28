db5fe86ed62f8acf54500693281fae3f29f0a79f
Matteo Regazzi <mregazzi@tiscali.it>
2021-06-28 22:32:39 +0200

    Now we have an example for the Command design pattern.

---
+++ src/design_patterns/command/CommandExample.java
@@ -0,0 +1,8 @@
+package design_patterns.command;
+
+public class CommandExample {
+    public static void main(String[] args) {
+        Editor editor = new Editor();
+        editor.init();
+    }
+}
